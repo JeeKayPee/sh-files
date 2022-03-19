7dda1dde7a5d07c25c10f594fd30cb52ebb2ecf0
Gaurav Pandey <45666264+JeeKayPee@users.noreply.github.com>
2022-03-19 14:18:32 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -2,3 +2,4 @@ sudo apt update
 sudo apt install docker.io --yes
 sudo apt install docker-compose.io --yes
 sudo docker pull bitnami/codeigniter
+sudo docker run bitnami/codeigniter -p 8080:8080 -p 80:80
